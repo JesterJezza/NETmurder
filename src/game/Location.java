11aaeba3466ec27e1b4766ed80885ce34470eee5
JesterJezza <jeremywilks@hotmail.co.uk>
2015-07-24 16:58:34 +0100

    Location and global change

--- src/game/Location.java
+++ src/game/Location.java
@@ -5,9 +5,16 @@ import java.util.ArrayList;
 public class Location 
 {
 	String name;
-	ArrayList<Location> connectedLocations;
+	boolean statAdj = false;
+	String stat1type = null;
+	int stat1val = 0;
+	String stat2type = null;
+	int stat2val = 0;
+	ArrayList<Location> connectedLocations = new ArrayList<Location>();
 	
 	ArrayList<Item> items;
+	
+	
 	
 	//Locations
 	//Board Room (MNTL UP)
@@ -16,4 +23,76 @@ public class Location
 	//Lift (SPD DOWN)
 	//M1 (CODE UP, MNTL UP)
 	//M2 (CODE UP, MNTL UP)
+	//Hallway
+	public static void init()
+	{
+		globals.b = Location("b");
+	}
+	
+	public Location(String locationName)
+	{
+		if (locationName == "b")
+		{
+			name = "Board Room";
+			statAdj = true;
+			stat1type = "mental";
+			stat1val = 1;
+			
+			connectedLocations.add(globals.b);
+			
+		}
+		else if (locationName == "p")
+		{
+			name = "Pool Table";
+			statAdj = true;
+			stat1type = "speed";
+			stat1val = 1;
+			
+			connectedLocations.add(globals.p);
+		
+		}
+		else if (locationName == "t")
+		{
+			name = "Toilets";
+			statAdj = true;
+			stat1type = "health";
+			stat1val = -1;
+			connectedLocations.add(globals.t);
+		}
+		else if (locationName == "l")
+		{
+			name = "Lift";
+			statAdj = true;
+			stat1type = "speed";
+			stat1val = -1;
+			connectedLocations.add(globals.l);
+		}
+		else if (locationName == "m1")
+		{
+			name = "Meeting Room 1";
+			statAdj = true; 
+			stat1type = "code";
+			stat1val = 1;
+			stat2type = "mental";
+			stat2val = -1;
+			connectedLocations.add(globals.m1);
+		}
+		else if (locationName == "m2")
+		{
+			name = "Meeting Room 2";
+			statAdj = true; 
+			stat1type = "code";
+			stat1val = 1;
+			stat2type = "mental";			
+			stat2val = -1;
+			connectedLocations.add(globals.m2);
+		}
+	}
+	
+	public Location()
+	{
+		name = "Hallway";
+		connectedLocations.add(globals.h);
+		//ArrayList<Location> connectedLocations
+	}
 }
--- src/game/globals.java
+++ src/game/globals.java
@@ -3,5 +3,19 @@ package game;
 public class globals 
 {	
 	public static String[] Attributes = { "Stength", "Speed", "Mental", "Fortitude", "Coding" };
+	
+	public static Location h = new Location();
+	public static Location b = new Location("b");
+	public static Location p = new Location("p");
+	public static Location t = new Location("t");
+	public static Location l = new Location("l");
+	public static Location m1 = new Location("m1");
+	public static Location m2 = new Location("m2");
+	
+	public static void init()
+	{
+		Location.init();
+		
+	}
 
 }
